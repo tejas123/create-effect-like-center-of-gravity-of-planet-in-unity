65364e623f4a3315b31e73abc29130e4f37b3157
Bharat <bharatappguru@gmail.com>
2015-02-17 13:59:52 +0530

    Create Effect Like Center of Gravity of Planet in Unity Tutorial

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour
+{
+
+        public Transform planet;
+		private float forceAmountForRotation = 10;
+		private Vector3 directionOfPlanetFromBird;
+        private bool allowForce;
+
+        void Start()
+        {
+            directionOfPlanetFromBird = Vector3.zero;
+        }
+
+		void Update ()
+		{
+
+            allowForce = false;
+
+            if (Input.GetKey(KeyCode.Space))
+                allowForce = true;
+
+            directionOfPlanetFromBird = transform.position - planet.position;
+            transform.right = Vector3.Cross(directionOfPlanetFromBird, Vector3.forward);
+		}
+
+		void FixedUpdate ()
+		{
+			if (allowForce) 
+				rigidbody2D.AddForce (transform.right * forceAmountForRotation);
+		}
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour
+{
+
+		public Transform bird;
+		private float gravitationalForce = 5;
+        private Vector3 directionOfBirdFromPlanet;
+
+		void Start ()
+		{
+				directionOfBirdFromPlanet = Vector3.zero;
+		}
+
+		void FixedUpdate ()
+		{
+				directionOfBirdFromPlanet = (transform.position - bird.position).normalized;
+				bird.rigidbody2D.AddForce (directionOfBirdFromPlanet * gravitationalForce);			
+		}
+}
